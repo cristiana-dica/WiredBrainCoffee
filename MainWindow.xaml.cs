887b8844cee15ec076584e31f30df0e6b1fa343f
Cristiana <cristiana.dica@cncn.ro>
2024-11-11 09:48:50 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WiredBrainCoffee
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_Add_Customer(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonMovNavigation_Click(object sender, RoutedEventArgs e)
+        {
+            var column = Grid.GetColumn(customerListGrid);
+
+            var newColumn = column == 0 ? 2 : 0;
+
+            customerListGrid.SetValue(Grid.ColumnProperty, newColumn);
+
+        }
+    }
+}
